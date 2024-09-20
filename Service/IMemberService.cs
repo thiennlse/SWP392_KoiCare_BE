e1dd313f4c9b7d69de83e879ec9d31014cbbd8af
thiennlse <thiennlse172858@gmail.com>
2024-09-20 23:32:30 +0700

    feat:MemberService

---
+++ Service/IMemberService.cs
@@ -0,0 +1,18 @@
+﻿using BusinessObject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public interface IMemberService
+    {
+        Task<List<Member>> GetAllMember();
+        Task<Member> GetMemberById(int id);
+        Task<Member> Login(string email, string password);
+        Task Register(Member member);
+        Task<Member> UpdateMember(Member member);
+    }
+}
---
+++ Service/MemberService.cs
@@ -0,0 +1,46 @@
+﻿using BusinessObject.Models;
+using Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class MemberService : IMemberService
+    {
+        private readonly IMemberRepository _memberRepository;
+
+        public MemberService(IMemberRepository memberRepository)
+        {
+            _memberRepository = memberRepository;
+        }
+
+        public async Task<List<Member>> GetAllMember()
+        {
+            return await _memberRepository.GetAllMember();
+        }
+
+        public async Task<Member> GetMemberById(int id)
+        {
+           return await _memberRepository.GetMemberById(id);
+        }
+
+        public async Task<Member> Login(string email, string password)
+        {
+            return await _memberRepository.Login(email, password);
+        }
+
+        public async Task Register(Member member)
+        {
+            await _memberRepository.Register(member);
+        }
+
+        public async Task<Member> UpdateMember(Member member)
+        {
+            return await _memberRepository.UpdateMember(member);
+        }
+    }
+}
