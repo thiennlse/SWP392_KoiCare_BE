48f714186d70e2f9131e53aa2f85eee1457ba0ae
Kim Thanh Trần <xitrum_1902@yahoo.com>
2024-10-01 13:41:14 +0700

    Edit: OrderController, PoolController, ProductController

--- KoiCareApi/Controllers/OrderController.cs
+++ KoiCareApi/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BusinessObject.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Service;
 
@@ -9,5 +10,92 @@ namespace KoiCareApi.Controllers
     public class OrderController : ControllerBase
     {
         private readonly IOrderService _orderService;
+
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllOrder()
+        {
+            var order = await _orderService.GetAllOrder();
+            if (order == null)
+            {
+                return NotFound("empty Order");
+            }
+            return Ok(order);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOrderById(int id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest("phease input id >0");
+            }
+            var _order = await _orderService.GetOrderById(id);
+            if (_order == null)
+            {
+                return NotFound("order is not exit");
+            }
+            return Ok(_order);
+        }
+
+        [HttpPost("add")]
+        public async Task<IActionResult> AddNewOrder([FromBody] OrderRequestModel _order)
+        {
+            if (_order == null)
+            {
+                return BadRequest("please input order information");
+            }
+            Order order = new Order();
+            order.FoodId = _order.FoodId;
+            order.PoolId = _order.PoolId;
+            order.Name = _order.Name;
+            order.Image = _order.Image;
+            order.Size = _order.Size;
+            order.Weight = _order.Weight;
+            order.Age = _order.Age;
+            order.Gender = _order.Gender;
+            order.Origin = _order.Origin;
+            await _orderService.AddNewOrder(order);
+            return Created("Created", order);
+        }
+
+        [HttpDelete("Delete")]
+        public async Task<IActionResult> DeleteById(int id)
+        {
+            var _order = await _orderService.GetOrderById(id);
+            if (_order == null)
+            {
+                return NotFound("order is not exits");
+            }
+            await _orderService.DeleteOrder(id);
+            return NoContent();
+        }
+
+        [HttpPatch("update/{id}")]
+        public async Task<IActionResult> UpdateById([FromBody] OrderRequestModel _order, int id)
+        {
+            var order = await _orderService.GetOrderById(id);
+            if (_order == null)
+            {
+                return NotFound("order is not exits");
+            }
+            order.Id = id;
+            order.FoodId = _order.FoodId;
+            order.PoolId = _order.PoolId;
+            order.Name = _order.Name;
+            order.Image = _order.Image;
+            order.Size = _order.Size;
+            order.Weight = _order.Weight;
+            order.Age = _order.Age;
+            order.Gender = _order.Gender;
+            order.Origin = _order.Origin;
+
+            await _orderService.UpdateById(order);
+            return Ok(order);
+        }
     }
 }
--- KoiCareApi/Controllers/ProductController.cs
+++ KoiCareApi/Controllers/ProductController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BusinessObject.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Service;
 
 namespace KoiCareApi.Controllers
 {
@@ -7,5 +9,92 @@ namespace KoiCareApi.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProduct()
+        {
+            var product = await _productService.GetAllProduct();
+            if (product == null)
+            {
+                return NotFound("empty Product");
+            }
+            return Ok(product);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductById(int id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest("phease input id >0");
+            }
+            var _product = await _productService.GetProductById(id);
+            if (_product == null)
+            {
+                return NotFound("Product Does not exit");
+            }
+            return Ok(_product);
+        }
+
+        [HttpPost("add")]
+        public async Task<IActionResult> AddNewProduct([FromBody] ProductRequestModel _product)
+        {
+            if (_product == null)
+            {
+                return BadRequest("please input product information");
+            }
+            Product product = new Product();
+            product.Id = _product.Id;
+            product.UserId = _product.UserId;
+            product.Name = _product.Name;
+            product.Cost = _product.Cost;
+            product.Description = _product.Description;
+            product.Origin = _product.Weight;
+            product.Productivity = _product.Productivity;
+            product.Code = _product.Code;
+            product.InStock = _product.InStock;
+            await _productService.AddNewProduct(product);
+            return Created("Created", product);
+        }
+
+        [HttpDelete("Delete")]
+        public async Task<IActionResult> DeleteById(int id)
+        {
+            var _product = await _productService.GetProductById(id);
+            if (_product == null)
+            {
+                return NotFound("product is not exits");
+            }
+            await _productService.DeleteProduct(id);
+            return NoContent();
+        }
+
+        [HttpPatch("update/{id}")]
+        public async Task<IActionResult> UpdateById([FromBody] ProductRequestModel _product, int id)
+        {
+            var product = await _productService.GetProductById(id);
+            if (_product == null)
+            {
+                return NotFound("product is not exits");
+            }
+            product.Id = _product.Id;
+            product.UserId = _product.UserId;
+            product.Name = _product.Name;
+            product.Cost = _product.Cost;
+            product.Description = _product.Description;
+            product.Origin = _product.Weight;
+            product.Productivity = _product.Productivity;
+            product.Code = _product.Code;
+            product.InStock = _product.InStock;
+
+            await _productService.UpdateProduct(product);
+            return Ok(product);
+        }
     }
 }
