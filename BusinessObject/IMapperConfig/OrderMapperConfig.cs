a4c91e391aecf34174eba53929d057b348ae4b11
xitrum1902 <kimttse63081@fpt.edu.vn>
2024-10-04 13:38:10 +0700

    Edit: OrderMapperConfig, ProductMapperConfig

--- BusinessObject/IMapperConfig/OrderMapperConfig.cs
+++ BusinessObject/IMapperConfig/OrderMapperConfig.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AutoMapper;
+using BusinessObject.Models;
+using BusinessObject.ResponseModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,11 @@ using System.Threading.Tasks;
 
 namespace BusinessObject.IMapperConfig
 {
-    internal class OrderMapperConfig
+    public class OrderMapperConfig
     {
+        public static void CreateMap(IMapperConfigurationExpression cfg)
+        {
+            cfg.CreateMap<Order, OrderResponseModel>();
+        }
     }
 }
