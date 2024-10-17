c78cecf0f694eb990b0c567996cb3d942cc247c2
thiennlse <thiennlse172858@gmail.com>
2024-10-17 22:00:13 +0700

    feat: upload to Docker

--- BusinessObject/Migrations/20241017135515_CreateInitial.Designer.cs
+++ BusinessObject/Migrations/20241017135515_CreateInitial.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BusinessObject.Migrations
 {
     [DbContext(typeof(KoiCareDBContext))]
-    [Migration("20241011030829_InitialCreate")]
-    partial class InitialCreate
+    [Migration("20241017135515_CreateInitial")]
+    partial class CreateInitial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
