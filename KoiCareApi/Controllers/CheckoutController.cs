ac566c0048d13b00b3609aa4e15e3e1fdbfe28c7
khoaNamK3 <namsugarcake@gmail.com>
2024-10-19 20:58:58 +0700

    Update checkout

--- KoiCareApi/Controllers/CheckoutController.cs
+++ KoiCareApi/Controllers/CheckoutController.cs
@@ -18,43 +18,45 @@ namespace KoiCareApi.Controllers
         private readonly IOrderService _orderService;
         private readonly IProductService _productService;
 
-        public CheckoutController(PayOS payOS, IPaymentService paymentService, IOrderService orderService, IProductService productService) 
+        public CheckoutController(PayOS payOS, IPaymentService paymentService, IOrderService orderService, IProductService productService)
         {
             _payOS = payOS;
-            _paymentService = paymentService;   
+            _paymentService = paymentService;
             _orderService = orderService;
             _productService = productService;
         }
 
         [HttpPost("create-payment-link")]
-        public async Task<IActionResult> Checkout([FromBody] PaymentRequestModel orderRequest) 
+        public async Task<IActionResult> Checkout([FromBody] PaymentRequestModel orderRequest)
         {
             try
             {
                 List<ItemData> items = new List<ItemData>();
                 int ordercode = int.Parse(DateTimeOffset.Now.ToString("ffffff"));
-              
+
 
                 List<int> productList = orderRequest.orderId.ToList();
                 foreach (int id in productList)
                 {
-                    Product product =  await _productService.GetProductById(id);
+                    Product product = await _productService.GetProductById(id);
                     ItemData item = new ItemData(product.Name, 1, (int)orderRequest.TotalCost);
                     items.Add(item);
-                }  
-                //(int)(orderRequest.TotalCost*100)
-                PaymentData paymentData = new PaymentData(ordercode,(int)orderRequest.TotalCost,"payment order",items,orderRequest.cancelUrl,orderRequest.returnUrl);
+                }
+                
+                PaymentData paymentData = new PaymentData(ordercode, (int)orderRequest.TotalCost, "payment order", items, orderRequest.cancelUrl, orderRequest.returnUrl);
 
                 CreatePaymentResult paymentResult = await _paymentService.createPaymentLink(paymentData);
                 return Ok(paymentResult.checkoutUrl);
 
 
             }
-            catch (Exception ex) 
+            catch (Exception ex)
             {
-            return BadRequest(ex.Message);
+                return BadRequest(ex.Message);
             }
         }
 
+       
+      
     }
 }
