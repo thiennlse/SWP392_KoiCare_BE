89f22d48cca2720f68820c077a2e55e89a3e5d55
xitrum1902 <kimttse63081@fpt.edu.vn>
2024-10-31 21:06:03 +0700

    Create: ChatGPTRequestModel and ChatGPTResponseModel

---
+++ BusinessObject/RequestModel/ChatGPTRequestModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject.RequestModel
+{
+    public class ChatGPTRequestModel
+    {
+        public string Model { get; set; } = "gpt-3.5-turbo"; // You can change this to "gpt-4" or other models
+        public List<Message> Messages { get; set; } = new List<Message>();
+
+        public ChatGPTRequest(string userInput)
+        {
+            Messages.Add(new Message { Role = "system", Content = "You are a helpful assistant." }); // Optional system message
+            Messages.Add(new Message { Role = "user", Content = userInput });
+        }
+    }
+
+    public class Message
+    {
+        public string Role { get; set; } // "user" or "assistant"
+        public string Content { get; set; }
+    }
+}
---
+++ BusinessObject/ResponseModel/ChatGPTResponseModel.cs
@@ -0,0 +1,22 @@
+﻿using BusinessObject.RequestModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BusinessObject.ResponseModel
+{
+    public class ChatGPTResponseModel
+    {
+        public List<Choice> Choices { get; set; }
+    }
+
+    public class Choice
+    {
+        public int Index { get; set; }
+        public Message Message { get; set; }
+        public string FinishReason { get; set; }
+    }
+}
