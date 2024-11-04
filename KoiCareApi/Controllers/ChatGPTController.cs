437804129aa546e035ce18963f0ae15702712f04
xitrum1902 <kimttse63081@fpt.edu.vn>
2024-11-04 14:51:00 +0700

    Edit: BusinessObject, ChatGPTController, KoiCareAPI, Service

--- KoiCareApi/Controllers/ChatGPTController.cs
+++ KoiCareApi/Controllers/ChatGPTController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessObject.RequestModel;
 using BusinessObject.ResponseModel;
+using ChatGPT.Net;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Service;
@@ -24,6 +25,28 @@ namespace KoiCareApi.Controllers
         public string Version()
         {
             return _configuration["VERSION"];
+        }
+
+        [HttpPost("fixGrammar")]
+        public async Task<IActionResult> FixGrammar([FromBody] ChatGPTRequestModel request)
+        {
+            // retrieve ai key from configuration
+            var openAiKey = _configuration["OPENAI_API_KEY"];
+
+            if (openAiKey == null)
+            {
+                return NotFound("key not found");
+            }
+
+            var openai = new ChatGpt(openAiKey);
+
+            var fixedSentence = await openai.Ask($"Fix the following sentence for spelling and grammar: {request.userInput}");
+            if (fixedSentence == null)
+            {
+                return NotFound("unable to call chat gpt");
+            }
+
+            return Ok(new ChatGPTResponseModel() { Message = fixedSentence });
         }
     }
 }
