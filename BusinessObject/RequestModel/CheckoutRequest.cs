9d5a19a301c9817d1a458c36eafc6e6f610d8429
thiennlse <thiennlse172858@gmail.com>
2024-11-14 01:20:39 +0700

    feat: checkout for subcription

---
+++ BusinessObject/RequestModel/CheckoutRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject.RequestModel
+{
+    public class CheckoutRequest
+    {
+        public List<PaymentRequestModel> OrderRequest { get; set; }
+        public int? SubscriptionId { get; set; }
+        public string CancelUrl { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
